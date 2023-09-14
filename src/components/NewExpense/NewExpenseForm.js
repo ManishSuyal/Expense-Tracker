9ae1d5f39c665e8fff115fd094ead4fdc847b027
Manish <manish.suyal@edufied.edu.vn>
2023-09-15 00:03:09 +0530

    code updated

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -2,39 +2,47 @@ import { useState } from "react";
 import "./NewExpenseForm.css" ;
 
 const ExpenseForm = () => {
-const [ entered , setTitle ] = useState({ 
-    enteredTitle : '' ,
-     enteredAmount : '' ,
-      enteredDate : ''
-     })
+const [ enteredTitle , setTitle ] = useState('')
+    const [ enteredAmount , setAmount ] = useState('')
+        const [ enteredDate , setDate ] = useState('')
+
+
 // const [ enteredDate , setDate] = useState("")
 // const [ enteredAmount  , settitleAmount ] = useState("")
 const amountChangeHandler = (event) =>{
-    setTitle((prevState)=>{
-     return { ...prevState , enteredAmount: event.target.value}
-    })
+    setAmount(event.target.value)
 }
 const titleChangeHandler = (event) =>{
-    setTitle((prevState)=>{
-        return { ...prevState , enteredTitle: event.target.value}
-       })}
-const dateChangeHandler = (event) =>{
-    setTitle(   setTitle((prevState)=>{
-        return { ...prevState , enteredAmount: event.target.value}
-       }))
+    setTitle(event.target.value)
 }
+const dateChangeHandler = (event) =>{
+     setDate(event.target.value)
+}
+// another way of using only one function for all onclick events 
+// const inputHandler = (identifier , value) =>{
+//     if(identifier === "title"){
+//         setTitle(value)
+//     }else if( identifier === "date") {
+//       setDate(value)
+//     }else {
+//     setAmount(value)
+// }
+// }
     return (<form>
         <div className="new-expense__controls">
             <div className="new-expense__control"> 
-             <label>{entered[0]}</label>
+             <label>{enteredTitle}</label>
              <input type="text" onChange={titleChangeHandler} />
+             {/* Below the input handler function only called when the onchange event occurs  */}
+             {/* <input type="text" onChange={(event)=> inputHandler('title', event.target.value)} /> */}
+
             </div>
             <div className="new-expense__control"> 
-             <label>{entered[1]}</label>
+             <label>{enteredAmount}</label>
              <input type="number" min='0.1' step='0.01' onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control"> 
-             <label>{entered[2]}</label>
+             <label>{enteredDate}</label>
              <input type="date" min='30-08-2022' max='30-12-2023' onChange={dateChangeHandler}/>
             </div>
         </div>
